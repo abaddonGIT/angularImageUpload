741035f1c2eab75f58425dbe2522445c5343876c
abaddonGIT <abaddongit@gmail.com>
2013-07-15 15:47:30 +0400

    alfa 0.0.1

--- js/miniUpload.js
+++ js/miniUpload.js
@@ -1,23 +1,30 @@
+/******************************************************
+ * Copyright 2013 by Abaddon <abaddongit@gmail.com>
+ * @author Abaddon <abaddongit@gmail.com>
+ * @version 1.0.0
+ * ***************************************************/
 /*global window, $, jQuery, document */
 (function($) {
     "use strict";
-    var totalBytesUploaded, totalBytes, queue = 0;
+    var totalBytesUploaded, totalBytes, queue, config, point, fileQueue;
+    //Если такое свойство не определено то добавляем его
+    if (Array.in_array === undefined) {
+        Array.prototype.in_array = function(str) {
+            var _arrayLenght = this.length,
+                    i;
 
-    Array.prototype.in_array = function(str) {
-        var _arrayLenght = this.length,
-                i;
-
-        for (i = _arrayLenght; i--; ) {
-            if (this[i] === str) {
-                return true;
+            for (i = _arrayLenght; i--; ) {
+                if (this[i] === str) {
+                    return true;
+                }
             }
-        }
-        return false;
-    };
+            return false;
+        };
+    }
 
     function miniUplode(el, def) {
-        this.el = el;
-        this.def = def;
+        point = el;
+        config = def;
         //Проверка на поддержку API
         if (window.File && window.FileList && window.FileReader) {
             this.html5Init();
@@ -26,114 +33,159 @@
             this.normalInit();
         }
     }
+    ;
+    //Скрываем поле input и рисуем кнопку
     miniUplode.prototype.buildUI = function() {
-        var def = this.def;
-        //Ставим нашему инпуту параметр accept для конкретных типов файлов
-        $(this.el).prop('accept', def.fileType).css({
-            'opacity': 0,
-            'width': def.width,
-            'cursor': 'pointer',
+        $(point).prop('accept', config.fileType).css({
             'position': 'absolute',
+            'z-index': 2,
+            'width': 600,
+            'opacity': 0,
+            'font-size': '150px',
             'top': 0,
-            'left': 0,
-            'padding': def.padding
+            'left': -20,
+            'padding': 0,
+            'margin': 0,
+            'cursor': 'pointer',
+            '-moz-opacity': 0,
+            'filter': 'alpha(opacity=0)'
         });
-        parent = $(this.el).wrap("<div id=" + def.butId + "></div>").parent();
-        parent.css({'width': def.width,
+
+        $(config.contenerID).css({
             'position': 'relative',
-            'text-align': 'center',
-            'padding': def.padding,
-            'cursor': 'poiter'
-        }
-        ).append(def.buttonTitle);
-    };
-    //Кроссбраузерное создание запроса
-    miniUplode.prototype.getXmlHttp = function() {
-        var xhr;
-        try {
-            xhr = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try {
-                xhr = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (E) {
-                xhr = false;
-            }
-        }
-        if (!xhr && typeof XMLHttpRequest !== 'undefined') {
-            xhr = new XMLHttpRequest();
-        }
-        return xhr;
+            'overflow': 'hidden',
+            'width': config.width,
+            'padding': config.padding,
+            'cursor': 'pointer',
+            'text-align': 'center'
+        }).append(config.buttonTitle);
+
+
     };
     //Загрузка файлов при помощи HTML5 API
     miniUplode.prototype.html5Init = function() {
-        var def = this.def, up = this, parent;
+        var up = this, parent;
         this.buildUI();
         //Вешаем обработчик на изменение в поле выбора фоток
-        this.el.addEventListener('change', function() {
-            var fCount = this.files.length, queue = 0, i = 0, j = 0, //кол-во выбранных файлов
-                    acceptFiles = def.fileType.split(',');
+        point.addEventListener('change', function() {
+            var fCount = this.files.length, i = 0, j = 0, //кол-во выбранных файлов
+                    acceptFiles = config.fileType.split(',');
+
+            queue = 0, totalBytes = 0, totalBytesUploaded = 0, fileQueue = [];
 
             //Строим очередь куда попадут файлы только с нужным типом
             for (; i < fCount; i++) {
                 if (acceptFiles.in_array(this.files[i].type)) {
-                    queue++;
-                }
-                else {
-                    //Удаляем элементы, которые не прошли проверку
-                    delete this.files[i];
-                    console.log(this.files);
+                    fileQueue[j] = this.files[i];
+                    totalBytes += this.files[i].size;
+                    j++;
                 }
             }
-
-            for (; j < queue; j++) {
-                //Запускаем ф-ю перед загрузкой файла
-                def.beforeFileUplode(this.files[j]);
-                //загружаем фотки
-                up.uplode(this.files[j]);
-            }
-
+            //запускаем загрузку
+            uplode(fileQueue);
         }, false);
     };
+
+    function progressHandlingFunction(e, file) {
+
+        if (e.lengthComputable) {
+            var lapsedBytes = 0;
+            if (e.total === e.loaded) {
+                totalBytesUploaded += e.loaded;
+
+                lapsedBytes = e.loaded - file.size;
+
+                totalBytesUploaded = totalBytesUploaded - lapsedBytes;
+
+                config.uploadProgress(totalBytesUploaded, totalBytes, lapsedBytes);
+            }
+        }
+    }
+
     //Загрузка файла на сервер
-    miniUplode.prototype.uplode = function(file) {
-        var xhr = this.getXmlHttp(), def = this.def, form = new FormData();
-        form.append('path', '/');
-        form.append('file', file);
-        //Тут запускаем ф-ю, которая расчитывает прогресс бар
-        xhr.upload.addEventListener('progress', function(e) {
-            var percent = parseInt(e.loaded / e.total * 100);
-            $('#progress').text('Загрузка: ' + percent + '%');
-        }, false);
-        //Проверяем выполнился ли запрос
-        xhr.onreadystatechange = function(e) {
-            if (e.target.readyState === 4) {
-                if (e.target.status === 200) {
-                    def.fileUplode(file);
-                }
-                else {
-                    throw('Файл не был загружен');
-                }
+    function uplode(files) {
+        var ln = files.length;
+
+        //console.log(ln);
+
+        var _uplode = function(files, ln, i) {//рекурсивная ф-я для загрузки файлов
+
+            if (i === undefined) {
+                i = 0;
+            }
+
+            if (i < ln) {
+                var form = new FormData();
+                form.append('file', files[i]);
+
+                $.ajax({
+                    url: config.uploadURL,
+                    type: 'POST',
+                    xhr: function() {
+                        var myXhr = $.ajaxSettings.xhr();
+                        if (myXhr.upload) { // проверка что осуществляется upload
+                            myXhr.upload.addEventListener('progress', function() {
+                                progressHandlingFunction(event, files[i]);
+                            }, false); //передача в функцию значений
+                        }
+                        myXhr.onreadystatechange = function() {
+                            if (this.readyState === 4) {
+                                if (this.status === 200) {
+
+                                } else {
+                                    throw ('Файл ' + files[i].name + ' не загружен!!!');
+                                }
+                            }
+                        };
+                        return myXhr;
+                    },
+                    data: form,
+                    dataType: config.type,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    beforeSend: function() {
+                        //Выполняется перед загрузкой файла
+                        config.beforeFileUplode(files[i], ln, i);
+                    },
+                    success: function(data) {
+                        //Выполняется после загрузки файла
+                        config.fileUploded(files[i], data);
+                        _uplode(files, ln, ++i);
+                    }
+                });
+            }
+            else {
+                //Вызывается после загрузки всех файлов
+                config.allFilesUploded(files, ln);
             }
         };
-        xhr.open('POST', def.uploadURL, true);
-        //Заголовки
-        xhr.setRequestHeader("X-File-Type", file.type);
-        xhr.setRequestHeader("X-File-Name", file.name);
-        xhr.send(form);
-    };
+        //console.log(ln);
+        _uplode(files, ln);
+    }
+    ;
 
     miniUplode.prototype.normalInit = function() {
+        this.buildUI();
         console.log('normal');
     };
 
     $.fn.miniUpload = function(options) {
         var def = {
             'beforeFileUplode': function(file) {
-            },
-            'fileUplode': function(file) {
 
             },
-            'progress': function() {
+            'fileUploded': function(file, data) {
+
+            },
+            'uploadProgress': function(totalBytesUploaded, totalBytes) {
+                //console.log(lapsedBytes);
+                var progress = ((totalBytesUploaded / totalBytes) * 100) + '%';
+                $(def.progressIdBlock).animate({
+                    'width': progress
+                }, 50);
+            },
+            'allFilesUploded': function(files, ln) {
 
             },
             'fileType': 'image/png,image/jpeg,image/gif',
@@ -141,8 +193,9 @@
             'dataType': 'html',
             'buttonTitle': 'Загрузить файлы',
             'width': 200,
-            'butId': 'fileUplode',
-            'padding': '10px'
+            'contenerID': '#uplodeBlock',
+            'padding': '10px',
+            'progressIdBlock': '#pr_bar'
         }, uplode;
 
         $.extend(def, options);
